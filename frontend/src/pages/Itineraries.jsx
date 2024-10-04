fbb6965830ecfc0104d1181730dfbf970709969e
Bishbishy2003 <bishbishy9@gmail.com>
2024-10-04 23:47:00 +0300

    fixed sort

--- frontend/src/pages/Itineraries.jsx
+++ frontend/src/pages/Itineraries.jsx
@@ -1,4 +1,11 @@
 const Itineraries = () =>{
 
+    return(
+<div>
+    <button onClick={sortasc}>Sort by price asc</button>
+    <button onClick={sortdsc}>Sort by price dsc</button>
+</div>
+        
+    )
 }
 export default Itineraries;
