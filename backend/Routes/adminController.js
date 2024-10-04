5962d6180f70b3f00f6d33318ec19020238cc0a3
mariam-amrr <161968052+mariam-amrr@users.noreply.github.com>
2024-10-04 15:18:26 +0300

    updated getproduct method in seller/tourist/ admin controllers

--- backend/Routes/adminController.js
+++ backend/Routes/adminController.js
@@ -99,24 +99,18 @@ const createProduct = async (req, res) => {
     }
   };
  
-  const getProduct = async (req, res) => {
-    const {productName} = req.body; // Use Name as a parameter to find the category
-    try {
-        if (productName) {
-            const Product = await productModel.findOne({ productName });
-            if (!Product) {
-                return res.status(404).json({ msg: "Product not found" });
-            }
-            res.status(200).json(Product);
-        } else {
-            const Products = await productModel.find();
-            res.status(200).json(Products); // Return all categories if no Name is provided
-        }
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
 
+const getProduct = async (req, res) => {
+  const { productName } = req.query;
+
+  try {
+      const product = await productModel.findOne({ productName: productName });
+      res.status(200).json(product);
+  } 
+  catch (error) {
+      res.status(400).json({ error: error.message });
+  }
+};
 
 const deleteAdvertiser = async (req, res) => {
     const { Username } = req.body;  // Getting Username from req.body
