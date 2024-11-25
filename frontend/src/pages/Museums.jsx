b9d6bc1fe9fd07f203e3467c941c5e06d940f064
ranaelsharkawy23 <ranaahmedsharkawy@icloud.com>
2024-11-25 18:34:53 +0200

    museum style

--- frontend/src/pages/Museums.jsx
+++ frontend/src/pages/Museums.jsx
@@ -1,6 +1,24 @@
 import React, { useState, useEffect } from 'react'; 
 import museumHistoryImage from '../images/museumhistory.jpg'; // Adjust the path as needed
-import { FaShareAlt } from "react-icons/fa";
+import IosShareIcon from '@mui/icons-material/IosShare';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import { FiCopy } from 'react-icons/fi'; // Import a copy icon
+import romanImage from '../images/roman.jpg'; // Adjust the path as needed
+import museummImage from '../images/museumm.jpg'; // Adjust the path as needed 
+import buckinghamImage from '../images/buckingham.jpg'; // Adjust the path as needed
+import fasImage from '../images/fas.jpg'; // Adjust the path as needed
+import pyramidsImage from '../images/pyramids.jpg'; // Adjust the path as needed
+import logo from '../images/image.png'; // Adjust the path based on your folder structure
+import { FaBars, FaTimes,FaHome, FaUser, FaMapMarkerAlt } from "react-icons/fa"; // For menu icons
+import { FaLandmark, FaUniversity, FaBox, FaMap, FaRunning, FaBus, FaPlane, FaHotel, FaShoppingCart,
+  FaClipboardList,
+  FaStar, } from "react-icons/fa";
+import { FaPercentage, FaCalendarAlt, FaTag ,FaUserCircle} from 'react-icons/fa';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+
+
+
+
 
 const Museums = () => {
   const [museums, setMuseums] = useState([]);
@@ -15,7 +33,9 @@ const Museums = () => {
   const [email, setEmail] = useState('');
   const [isEmailMode, setIsEmailMode] = useState(false);
   const [museumToShare, setMuseumToShare] = useState(null);
+  const [showEmailInput, setShowEmailInput] = useState(false);
   
+  const navigate = useNavigate();
 
   // Fetch museums from the backend
   const handleViewAllMuseums = async () => {
@@ -119,6 +139,7 @@ const Museums = () => {
           await navigator.clipboard.writeText(data.link); // Copy link to clipboard
           setCopySuccess((prev) => ({ ...prev, [museumName]: 'Link copied to clipboard!' })); // Set success message for the specific museum
         } else if (shareMethod === 'email') {
+      
           alert('Link sent to the specified email!');
         }
       } else {
@@ -135,11 +156,24 @@ const Museums = () => {
   const handleShareMode = (museum) => {
     setMuseumToShare(museum);
     setIsEmailMode(!isEmailMode);
+    setShowEmailInput(true);
   };
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>Museums</h1>
-      <button onClick={handleViewAllMuseums} style={styles.button}>
+      {/* Header Section */}
+    <header style={styles.header}>
+  <div style={styles.logoContainer}>
+    <img src={logo} alt="Logo" style={styles.logo} />
+  </div>
+  <h1 style={styles.title}>Museums</h1>  <FaUserCircle
+    alt="Profile Icon"
+    style={styles.profileIcon}
+    // Navigate to profile
+  />
+
+</header>
+      
+      <button onClick={handleViewAllMuseums} style={styles.buttonsearch}>
         View All Museums
       </button>
 
@@ -204,10 +238,24 @@ const Museums = () => {
           {museums.map((museum) => (
             <div key={museum._id} style={styles.card}>
               <img
-                src={museum.Name === 'National Museum of History' ? museumHistoryImage : museum.mainImage}
-                alt={museum.Name}
-                style={styles.image}
-              />
+        src={
+          museum.Name === "Roman Museum"
+            ? romanImage
+            : museum.Name === "National Museum of History"
+            ? museumHistoryImage
+            : museum.Name === "museum"
+            ? museummImage
+            : museum.Name === "buckingham palace"
+            ? buckinghamImage 
+            :museum.Name === "fas"
+            ? fasImage
+            :museum.Name === "pyramids"
+            ? pyramidsImage
+            : museum.mainImage
+        }
+        alt={museum.Name}
+        style={styles.image}
+      />
               <div style={styles.details}>
                 <h2 style={styles.museumName}>{museum.Name}</h2>
                 <p style={styles.description}>{museum.Description}</p>
@@ -217,51 +265,48 @@ const Museums = () => {
     onClick={() => handleShareMode(museum)}
     style={styles.shareButton}
   >
-    <FaShareAlt />
+    <IosShareIcon />
   </button>
-                  <button
-                    onClick={() => handleShareMode(museum)}
-                    style={styles.shareButton}
-                  >
-                    <i className="fas fa-share-alt"></i>
-                  </button>
-
-                 {/* Dropdown */}
-                {isEmailMode &&
-                  museumToShare &&
-                  museumToShare._id === museum._id && (
-                    <div style={styles.shareDropdown}>
-                      <button
-                        onClick={() => handleShare(museum.Name, 'copy')}
-                        style={styles.shareOption}
-                      >
-                        Copy Link
-                      </button>
-                      <button
-                        onClick={() => setIsEmailMode(false)}
-                        style={styles.shareOption}
-                      >
-                        Share via Email
-                      </button>
-
-                      {/* Email Input Field */}
-                      {museumToShare && museumToShare._id === museum._id && (
-                        <div style={{ marginTop: '10px' }}>
-                          <input
-                            type="email"
-                            placeholder="Enter recipient's email"
-                            value={email}
-                            onChange={handleEmailInputChange}
-                            style={styles.emailInput}
-                          />
-                          <button
-                            onClick={() => handleShare(museum.Name, 'email')}
-                            style={{ ...styles.button, marginTop: '10px' }}
-                          >
-                            Send Email
-                          </button>
-                        </div>
-                      )}
+  <button
+          onClick={() => handleShareMode(museum)}
+          style={styles.shareOption}
+        >
+          <i className="fas fa-share-alt" style={styles.shareOptionIcon}></i>
+        
+        </button>
+        {/* Dropdown */}
+        {isEmailMode && museumToShare && museumToShare._id === museum._id && (
+          <div style={styles.shareDropdown}>
+            <button
+              onClick={() => handleShare(museum.Name, "copy")}
+              style={styles.shareOption}
+            >
+              <FiCopy style={styles.shareOptionIcon} /> Copy Link
+            </button>
+            <button
+              onClick={() => setIsEmailMode(false)}
+              style={styles.shareOption}
+            >
+              <MailOutlineIcon style={styles.shareOptionIcon} /> Share via Email
+            </button>
+            {/* Email Input Field */}
+            {showEmailInput && museumToShare && museumToShare._id === museum._id && (
+              <div style={{ marginTop: "10px" }}>
+                <input
+                  type="email"
+                  placeholder="Enter recipient's email"
+                  value={email}
+                  onChange={handleEmailInputChange}
+                  style={styles.emailInput}
+                />
+                <button
+                  onClick={() => handleShare(museum.Name, "email")}
+                  style={{ ...styles.button, marginTop: "10px" }}
+                >
+                  Send Email
+                </button>
+              </div>
+            )}
                     </div>
                   )}
               </div>
@@ -283,6 +328,33 @@ const styles = {
     padding: '20px',
     fontFamily: 'Arial, sans-serif',
   },
+  header: {
+    position: 'fixed', // Make the header fixed
+    top: '0', // Stick to the top of the viewport
+    left: '0',
+    width: '100%', // Make it span the full width of the viewport
+    backgroundColor: '#0F5132', // Green background
+    color: 'white', // White text
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '10px 20px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // Add shadow for depth
+    zIndex: '1000', // Ensure it appears above other content
+  },
+  logoContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  logo: {
+    height: '70px',
+    width: '80px',
+    borderRadius: '10px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.4)',
+  },
+  profileIcon:{
+    fontSize: '40px',
+  },
   title: {
     textAlign: 'center',
     fontSize: '2.5rem',
@@ -298,11 +370,23 @@ const styles = {
     cursor: 'pointer',
     marginRight: '10px',
   },
+  buttonsearch: {
+    marginTop:'90px',
+    color:'#0F5132',
+    padding: '10px 20px',
+    backgroundColor: '#0F5132',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    marginRight: '10px',
+  },
   searchInput: {
     padding: '10px',
     fontSize: '16px',
     width: '300px',
     marginRight: '10px',
+
     border: '1px solid #ddd',
     borderRadius: '5px',
   },
@@ -338,18 +422,21 @@ const styles = {
       boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
       overflow: 'hidden',
       width: '100%',
-      maxWidth: '350px', // Adjust the width
+      maxWidth: "350px", // Adjust the width
+      padding: "10px", // Reduce padding for a shorter container
+      height: "450px",
       backgroundColor: '#fff',
       marginBottom: '20px',
     },
     image: {
       width: '100%', // Make the image take the full width of the card
-      height: '200px', // Adjust the height for better visibility
-      objectFit: 'cover', // Ensure the image scales correctly
+      height: '380px', // Adjust the height for better visibility
+      objectFit: 'full', // Ensure the image scales correctly
     },
     details: {
-      padding: '20px',
-      textAlign: 'center', // Center-align the text
+      position: "relative", // Ensure the dropdown is positioned relative to this container
+      padding: "20px",
+      textAlign: "center",
     },
     museumName: {
       fontSize: '1.5rem',
@@ -371,26 +458,36 @@ const styles = {
       marginTop: '10px',
     },
     shareDropdown: {
-      top: '40px',
-      left: '0',
-      backgroundColor: '#fff',
-      border: '1px solid #ddd',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      zIndex: '1000',
-      padding: '10px',
+      position: "absolute", // Position relative to parent
+      top: "-230px", // Distance from the button (adjust as needed)
+      left: "25%", // Center align with the button
+      transform: "translateX(-50%)", // Center alignment adjustment
+      backgroundColor: "#fff",
+      border: "1px solid #ddd",
+      borderRadius: "8px",
+      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+      zIndex: "1000",
+      padding: "10px",
+      width: "200px", // Fixed width
     },
+    
+    
     shareOption: {
-      display: 'block',
-      width: '100%',
-      padding: '10px',
-      textAlign: 'center',
-      backgroundColor: 'transparent',
-      border: 'none',
-      cursor: 'pointer',
-      fontSize: '1rem',
-      color: '#0F5132',
-      textDecoration: 'none',
+      display: "flex",
+      alignItems: "center", // Align icon and text vertically
+      justifyContent: "flex-start", // Align icon and text to the left
+      width: "100%",
+      padding: "10px",
+      backgroundColor: "transparent",
+      border: "none",
+      cursor: "pointer",
+      fontSize: "1rem",
+      color: "black", // Icon and text color
+      textDecoration: "none",
+    },
+    shareOptionIcon: {
+      marginRight: "8px", // Space between the icon and text
+      fontSize: "1.0rem", // Ensure both icons are the same size
     },
     };
     
