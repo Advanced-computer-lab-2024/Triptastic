2c0e8a70c2fb4c981ac2970c3fcb04f395beb48a
omarfarhoud <omarfarhoud2003@gmail.com>
2024-11-26 23:42:09 +0200

    fixed uncaught runtime error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import React, { useContext, useEffect, useState } from 'react';
+
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import TouristReg from './pages/touristReg';
 import TourGuideReg from './pages/tourGuideReg';
@@ -46,7 +48,9 @@ import { Elements } from '@stripe/react-stripe-js'; // Import Elements from Stri
 import { loadStripe } from '@stripe/stripe-js'; // Import loadStripe
 
 import AttendedActivitiesPage from './pages/AttendedActivities';
-const stripePromise = loadStripe('your-publishable-key-here');
+
+const stripePromise = loadStripe('pk_test_51QPR4CHmIrhpZsCUkNDleRBlZDavN3bJ0zSGja0odQdTv8F6suSp02Cyx2sOVDyTbxi4J7Yqrnj7kQMwlu2LbwaU00pnINWNUs');
+stripePromise.then((stripe) => console.log('Stripe initialized:', stripe));
 
 function Home() {
   const navigate = useNavigate();
@@ -54,14 +58,10 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        
-      <div className="header-container">
-  <button className="header-button" onClick={() => navigate('/adminLogin')}>Login as Admin</button>
-  <button className="header-button" onClick={() => navigate('/tgov-login')}>Login as Tourism Governor</button>
-</div>
-
- 
-        
+        <div className="header-container">
+          <button className="header-button" onClick={() => navigate('/adminLogin')}>Login as Admin</button>
+          <button className="header-button" onClick={() => navigate('/tgov-login')}>Login as Tourism Governor</button>
+        </div>
 
         <h1>Welcome to Triptastic!</h1>
         <p>Please register as one of the following:</p>
@@ -70,77 +70,76 @@ function Home() {
           <button onClick={() => navigate('/tour-guide-register')}>Register as Tour Guide</button>
           <button onClick={() => navigate('/advertiser-register')}>Register as Advertiser</button>
           <button onClick={() => navigate('/seller-register')}>Register as Seller</button>
-          <button  onClick={() => navigate('/Guest')}> Continue as Guest </button>
+          <button onClick={() => navigate('/Guest')}>Continue as Guest</button>
         </div>
-        
-        {/* Continue as Guest Button */}
-        
       </header>
     </div>
   );
 }
 
 function App() {
+  const [isStripeLoaded, setIsStripeLoaded] = useState(false);
+
+  useEffect(() => {
+    stripePromise.then(() => setIsStripeLoaded(true));
+  }, []);
+
+  if (!isStripeLoaded) {
+    return <div>Loading Stripe...</div>;  // Show a loading state until Stripe is loaded
+  }
+
   return (
     <CurrencyProvider>
-    <Elements stripe={stripePromise}>  {/* Wrap your app or specific routes with Elements provider */}
-
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tourist-register" element={<TouristReg />} />
-        <Route path="/tour-guide-register" element={<TourGuideReg />} />
-        <Route path="/advertiser-register" element={<AdvertiserReg />} />
-        <Route path="/seller-register" element={<SellerReg />} />
-        <Route path="/seller-profile" element={<SellerProfile />} />
-        <Route path="/advertiser-profile" element={<AdvertiserProfile />} />
-        <Route path="/tourist-profile" element={<TouristProfile />} />
-        <Route path="/tour-guide-profile" element={<TourGuideProfile />} />
-        <Route path="/historical-locations" element={<HistoricalLocations />} />
-        <Route path="/museums" element={<Museums />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/itineraries" element={<Itineraries />} />
-        <Route path="/adminLogin" element={<AdminLogin />} />
-        <Route path="/adminPage" element={<AdminPage />} />
-        <Route path="/tourism-gov" element={<TourismGov />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/guest" element={<Guest />} />      
-        <Route path="/tgov-login" element={<TourismGovLogin />} />
-        <Route path="/advertiser-Activities" element={<AdvertiserActivity />} />
-        <Route path="/gov-historical" element={<GovernorH />} />
-        <Route path="/gov-museum" element={<GovernorM />} />
-        <Route path="/my-locations" element={<MyLocations />} />
-        <Route path="/my-museums" element={<MyMuseums />} />
-        <Route path="/map-Picker" element={<MapPicker />} />
-        <Route path="/book-flights" element={<BookFlights />} />
-        <Route path="/book-hotels" element={<BookHotels />} />
-        <Route path="/book-transportation" element={<BookTransportation />} />
-        <Route path="/Complaints" element={<Complaints/>}/>
-        <Route path="/tourist-orders" element={<TouristOrders/>}/>
-        <Route path="/activities/:name" element={<ActivityDetail/>} />
-        <Route path="/docs" element={<Docs/>}/>        
-        <Route path="/itineraries/:id" element={<ItineraryDetail/>} />
-        <Route path="/Historical/:Name" element={<HistoricalDetail/>} />
-        <Route path="/Museum/:Name" element={<MuseumDetail/>} />
-        <Route path="/AttendedActivitiesPage" element={<AttendedActivitiesPage/>}/> 
-        <Route path="/Cart" element={<Cart/>} />
-        <Route path="/Wishlist" element={<Wishlist/>} />
-        <Route path="/PromoCodeForm" element={<PromoCodeForm />} />
-        <Route path="/TouristSettings" element={<TouristSettings/>} />
-        <Route path="/BookmarkedEvents" element={<BookmarkedEvents />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/guideReport" element={<GuideReport/>} />
-
-
-
-
-
-      </Routes>
-    </Router>
-    </Elements>  {/* Close Elements provider */}
-
+      <Elements stripe={stripePromise}>  {/* Wrap your app or specific routes with Elements provider */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tourist-register" element={<TouristReg />} />
+            <Route path="/tour-guide-register" element={<TourGuideReg />} />
+            <Route path="/advertiser-register" element={<AdvertiserReg />} />
+            <Route path="/seller-register" element={<SellerReg />} />
+            <Route path="/seller-profile" element={<SellerProfile />} />
+            <Route path="/advertiser-profile" element={<AdvertiserProfile />} />
+            <Route path="/tourist-profile" element={<TouristProfile />} />
+            <Route path="/tour-guide-profile" element={<TourGuideProfile />} />
+            <Route path="/historical-locations" element={<HistoricalLocations />} />
+            <Route path="/museums" element={<Museums />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/itineraries" element={<Itineraries />} />
+            <Route path="/adminLogin" element={<AdminLogin />} />
+            <Route path="/adminPage" element={<AdminPage />} />
+            <Route path="/tourism-gov" element={<TourismGov />} />
+            <Route path="/activities" element={<Activities />} />
+            <Route path="/guest" element={<Guest />} />      
+            <Route path="/tgov-login" element={<TourismGovLogin />} />
+            <Route path="/advertiser-Activities" element={<AdvertiserActivity />} />
+            <Route path="/gov-historical" element={<GovernorH />} />
+            <Route path="/gov-museum" element={<GovernorM />} />
+            <Route path="/my-locations" element={<MyLocations />} />
+            <Route path="/my-museums" element={<MyMuseums />} />
+            <Route path="/map-Picker" element={<MapPicker />} />
+            <Route path="/book-flights" element={<BookFlights />} />
+            <Route path="/book-hotels" element={<BookHotels />} />
+            <Route path="/book-transportation" element={<BookTransportation />} />
+            <Route path="/Complaints" element={<Complaints />} />
+            <Route path="/tourist-orders" element={<TouristOrders />} />
+            <Route path="/activities/:name" element={<ActivityDetail />} />
+            <Route path="/docs" element={<Docs />} />
+            <Route path="/itineraries/:id" element={<ItineraryDetail />} />
+            <Route path="/Historical/:Name" element={<HistoricalDetail />} />
+            <Route path="/Museum/:Name" element={<MuseumDetail />} />
+            <Route path="/AttendedActivitiesPage" element={<AttendedActivitiesPage />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="/Wishlist" element={<Wishlist />} />
+            <Route path="/PromoCodeForm" element={<PromoCodeForm />} />
+            <Route path="/TouristSettings" element={<TouristSettings />} />
+            <Route path="/BookmarkedEvents" element={<BookmarkedEvents />} />
+            <Route path="/payment" element={<PaymentPage />} />
+            <Route path="/guideReport" element={<GuideReport />} />
+          </Routes>
+        </Router>
+      </Elements>
     </CurrencyProvider>
-
   );
 }
 
